d37f62bdd60cdd41e653f22bdd81ce3837f1e473
Tenny <tennypa@gmail.com>
2018-11-18 21:06:02 +0530

    importing math function

---
+++ import.py
@@ -0,0 +1,8 @@
+''' Here we are caclculatin mathematical and scientific 
+values by importing math function ''' 
+
+import math #importing math function 
+print ("Sin value of 30 is: ",math.sin(30)) 
+print ("power of 2 to 3 is:",math.pow(2,3)) 
+#power of a,b is a raise to b
+print ("Square root of 16 is:",math.sqrt(16))
